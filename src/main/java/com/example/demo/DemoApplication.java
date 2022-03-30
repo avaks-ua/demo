59ff9d58dfdf778bc47f2ba71cfcc58dd67331ae
oleksii sahir <oleksii.sahir@auto1.com>
2022-03-30 10:55:03 +0300

    handling

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,22 @@
 package com.example.demo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.*;
 import javax.websocket.server.PathParam;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -22,6 +28,7 @@ public class DemoApplication {
 
 }
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 class UserController {
@@ -35,7 +42,16 @@ class UserController {
 
     @GetMapping("/user/{id}")
     public User getUser(@PathVariable("id") Integer id) {
-        return repo.findById(id).get();
+        return getUserById(id);
+    }
+
+    private User getUserById(Integer id) {
+        UserNotFoundException ex = new UserNotFoundException("User not found " + id);
+        Optional<User> user = repo.findById(id);
+        if (user.isEmpty()) {
+            throw ex;
+        }
+        return user.get();
     }
 
     @GetMapping("/users")
@@ -44,14 +60,44 @@ class UserController {
     }
 }
 
+@ControllerAdvice
+class CustomExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<CustomHttpResponse> handle(RuntimeException ex) {
+        return new ResponseEntity<>(
+                new CustomHttpResponse(ex.getMessage(), "404"),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+}
+
+@AllArgsConstructor
+@Data
+class CustomHttpResponse {
+
+    private String message;
+    private String status;
+
+}
+
+class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
+
 
 @Repository
 interface UserRepository extends JpaRepository<User, Integer> {
 
-    User finUserByName(String name);
 
 }
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Entity
 @Table(name = "user_table")
@@ -61,4 +107,6 @@ class User {
     private Integer id;
     @Column
     private String name;
+    @Column
+    private String email;
 }
